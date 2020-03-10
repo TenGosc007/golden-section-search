c4a401c61933196f8f4a2015ef02eae535822fa0
Damian Schmidt <damian.schmidt97@gmail.com>
2020-03-10 21:19:41 +0100

    Add setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Golden Section Search',
+    version='1.0.0',
+    author='Aleksander Atamańczuk, Damian Schmidt',
+    packages=find_packages(),
+    install_requires=[],
+)
