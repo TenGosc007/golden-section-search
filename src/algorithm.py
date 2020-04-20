54eb0319233ec8dbf724047652bf4974c0f9be5a
Damian Schmidt <damian.schmidt97@gmail.com>
2020-04-20 20:55:41 +0200

    Add test function for algorithm for n = 2

--- src/algorithm.py
+++ src/algorithm.py
@@ -84,13 +84,78 @@ class Algorithm:
                 print('x1, x2', self.vars_dict['x2_0'], self.vars_dict['x2_1'])
                 print('f(x1, x2) = ', f_pt4)
 
-            self.vars_dict['x1_0'] = self.vars_dict['a_0'] + (1 - self.k) * (self.vars_dict['b_0'] - self.vars_dict['a_0'])
+            self.vars_dict['x1_0'] = self.vars_dict['a_0'] + (1 - self.k) * (
+                    self.vars_dict['b_0'] - self.vars_dict['a_0'])
             self.vars_dict['x2_0'] = self.vars_dict['a_0'] + self.k * (self.vars_dict['b_0'] - self.vars_dict['a_0'])
 
-            self.vars_dict['x1_1'] = self.vars_dict['a_1'] + (1 - self.k) * (self.vars_dict['b_1'] - self.vars_dict['a_1'])
+            self.vars_dict['x1_1'] = self.vars_dict['a_1'] + (1 - self.k) * (
+                    self.vars_dict['b_1'] - self.vars_dict['a_1'])
             self.vars_dict['x2_1'] = self.vars_dict['a_1'] + self.k * (self.vars_dict['b_1'] - self.vars_dict['a_1'])
 
         return x1_min, x2_min
+
+    def find_minimum_test(self):
+        a1 = 0  # lower bound for variable x
+        b1 = 1  # upper bound for variable x
+        a2 = 0  # lower bound for variable y
+        b2 = 1  # upper bound for variable y
+        epsilon = 0.00000001  # termination criteria
+        tau = (sqrt(5) - 1) / 2  # golden number
+        k = 0  # number of iterations
+
+        x1 = a1 + (1 - tau) * (b1 - a1)
+        x2 = a1 + tau * (b1 - a1)
+        y1 = a2 + (1 - tau) * (b2 - a2)
+        y2 = a2 + tau * (b2 - a2)
+
+        ek = [x1, y1]  # Point A
+        fk = [x1, y2]  # Point C
+        hk = [x2, y1]  # Point B
+        gk = [x2, y2]  # Point D
+        fek = self.example_function_n2(x1, y1)
+        ffk = self.example_function_n2(x1, y2)
+        fhk = self.example_function_n2(x2, y1)
+        fgk = self.example_function_n2(x2, y2)  # function values at points A, B, C and D.
+
+        min1 = None
+        while sqrt(pow((b1 - a1), 2) + pow((b2 - a2), 2)) > epsilon:  # termination condition
+            k = k + 1
+            min1 = min([fek, fhk, ffk, fgk])
+
+            if min1 == fek:
+                b1 = x2
+                b2 = y2
+            elif min1 == ffk:
+                b1 = x2
+                a2 = y1
+            elif min1 == fgk:
+                a1 = x1
+                a2 = y1
+            elif min1 == fhk:
+                a1 = x1
+                b2 = y2
+
+            x1 = a1 + (1 - tau) * (b1 - a1)
+            x2 = a1 + tau * (b1 - a1)
+            y1 = a2 + (1 - tau) * (b2 - a2)
+            y2 = a2 + tau * (b2 - a2)
+            fek = self.example_function_n2(x1, y1)
+            ffk = self.example_function_n2(x1, y2)
+            fhk = self.example_function_n2(x2, y1)
+            fgk = self.example_function_n2(x2, y2)
+            min1 = min([fek, fhk, ffk, fgk])
+
+        if min1 == fek:
+            print(f'minimum at the point ({x1},{y1})')
+        elif min1 == ffk:
+            print(f'minimum at the point ({x1},{y2})')
+        elif min1 == fhk:
+            print(f'minimum at the point ({x2},{y1}))')
+        elif min1 == fgk:
+            print(f'minimum at the point ({x2},{y2})')
+
+        print(f'minimum value {min1}')
+        print(f'number of iterations {k}')
 
     @staticmethod
     def example_function_n1(x):
@@ -100,10 +165,11 @@ class Algorithm:
     @staticmethod
     def example_function_n2(x, y):
         """Function has got minimum at x = 0.5 and y = 0.5."""
-        return pow(x - 0.5, 4) + pow(y - 0.5, 4) + 2 * pow(x - 0.5, 2) + pow(y - 0.5, 2) + 4 * (x - 0.5) * (y - 0.5)
+        return pow(x - 0.5, 4) + pow(y - 0.5, 4) + 2 * pow(x - 0.5, 2) + 2 * pow(y - 0.5, 2) + 4 * (x - 0.5) * (y - 0.5)
 
 
 if __name__ == '__main__':
     alg = Algorithm([0, 0], [1, 1], 2)
     # print('Szukany punkt:', alg.find_minimum_value_n1())
-    print('Szukany punkt:', alg.find_minimum_value_n2())
+    # print('Szukany punkt:', alg.find_minimum_value_n2())
+    alg.find_minimum_matlab()
