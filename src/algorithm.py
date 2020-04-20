73718ca41c02820f2cacd1acbe6a1b1cbff236ab
Damian Schmidt <damian.schmidt97@gmail.com>
2020-04-20 21:32:02 +0200

    Add golden section search for n = 2

--- src/algorithm.py
+++ src/algorithm.py
@@ -20,12 +20,13 @@ class Algorithm:
         return vars_dict
 
     def find_minimum_value_n1(self):
-        x_min = 0
+        x_min = None
+        minimum = None
+
         while sqrt(pow(self.vars_dict['b_0'] - self.vars_dict['a_0'], 2)) > self.epsilon:
             fx1 = self.example_function_n1(self.vars_dict['x1_0'])
             fx2 = self.example_function_n1(self.vars_dict['x2_0'])
             self.iteration += 1
-            print('Iteracja: ', self.iteration)
             minimum = min([fx1, fx2])
 
             if minimum == fx1:
@@ -40,122 +41,66 @@ class Algorithm:
                         self.vars_dict['b_0'] - self.vars_dict['a_0'])
 
             x_min = (self.vars_dict['a_0'] + self.vars_dict['b_0']) / 2
-            print('a: ', self.vars_dict['a_0'], end='\t')
-            print('x1: ', self.vars_dict['x1_0'], end='\t')
-            print('x2: ', self.vars_dict['x2_0'], end='\t')
-            print('b: ', self.vars_dict['b_0'])
-            print(x_min, end='\n\n')
 
-        return x_min
+        print(f'Minimum at the point {x_min}')
+        print(f'Minimum value {minimum}')
+        print(f'Number of iterations {self.iteration}')
 
     def find_minimum_value_n2(self):
-        x1_min = 0
-        x2_min = 0
+        fek = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x1_1'])
+        ffk = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x2_1'])
+        fhk = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x1_1'])
+        fgk = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x2_1'])
+
+        minimum = None
         while sqrt(pow(self.vars_dict['b_0'] - self.vars_dict['a_0'], 2) + pow(
                 self.vars_dict['b_1'] - self.vars_dict['a_1'], 2)) > self.epsilon:
-
             self.iteration += 1
-            print('Iteracja: ', self.iteration)
+            minimum = min([fek, fhk, ffk, fgk])
 
-            f_pt1 = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x1_1'])
-            f_pt2 = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x2_1'])
-            f_pt3 = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x1_1'])
-            f_pt4 = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x2_1'])
-
-            minimum = min([f_pt1, f_pt2, f_pt3, f_pt4])
-            if minimum == f_pt1:
+            if minimum == fek:
                 self.vars_dict['b_0'] = self.vars_dict['x2_0']
                 self.vars_dict['b_1'] = self.vars_dict['x2_1']
-                print('x1, x2', self.vars_dict['x1_0'], self.vars_dict['x1_1'])
-                print('f(x1, x2) = ', f_pt1)
-            elif minimum == f_pt2:
+            elif minimum == ffk:
                 self.vars_dict['b_0'] = self.vars_dict['x2_0']
                 self.vars_dict['a_1'] = self.vars_dict['x1_1']
-                print('x1, x2', self.vars_dict['x1_0'], self.vars_dict['x2_1'])
-                print('f(x1, x2) = ', f_pt2)
-            elif minimum == f_pt3:
+            elif minimum == fgk:
                 self.vars_dict['a_0'] = self.vars_dict['x1_0']
                 self.vars_dict['a_1'] = self.vars_dict['x1_1']
-                print('x1, x2', self.vars_dict['x2_0'], self.vars_dict['x1_1'])
-                print('f(x1, x2) = ', f_pt3)
-            else:
+            elif minimum == fhk:
                 self.vars_dict['a_0'] = self.vars_dict['x1_0']
                 self.vars_dict['b_1'] = self.vars_dict['x2_1']
-                print('x1, x2', self.vars_dict['x2_0'], self.vars_dict['x2_1'])
-                print('f(x1, x2) = ', f_pt4)
 
             self.vars_dict['x1_0'] = self.vars_dict['a_0'] + (1 - self.k) * (
                     self.vars_dict['b_0'] - self.vars_dict['a_0'])
             self.vars_dict['x2_0'] = self.vars_dict['a_0'] + self.k * (self.vars_dict['b_0'] - self.vars_dict['a_0'])
-
             self.vars_dict['x1_1'] = self.vars_dict['a_1'] + (1 - self.k) * (
                     self.vars_dict['b_1'] - self.vars_dict['a_1'])
             self.vars_dict['x2_1'] = self.vars_dict['a_1'] + self.k * (self.vars_dict['b_1'] - self.vars_dict['a_1'])
 
-        return x1_min, x2_min
+            fek = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x1_1'])
+            ffk = self.example_function_n2(self.vars_dict['x1_0'], self.vars_dict['x2_1'])
+            fhk = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x1_1'])
+            fgk = self.example_function_n2(self.vars_dict['x2_0'], self.vars_dict['x2_1'])
 
-    def find_minimum_test(self):
-        a1 = 0  # lower bound for variable x
-        b1 = 1  # upper bound for variable x
-        a2 = 0  # lower bound for variable y
-        b2 = 1  # upper bound for variable y
-        epsilon = 0.00000001  # termination criteria
-        tau = (sqrt(5) - 1) / 2  # golden number
-        k = 0  # number of iterations
+            minimum = min([fek, fhk, ffk, fgk])
 
-        x1 = a1 + (1 - tau) * (b1 - a1)
-        x2 = a1 + tau * (b1 - a1)
-        y1 = a2 + (1 - tau) * (b2 - a2)
-        y2 = a2 + tau * (b2 - a2)
+        x1_0 = self.vars_dict['x1_0']
+        x2_0 = self.vars_dict['x2_0']
+        x1_1 = self.vars_dict['x1_1']
+        x2_1 = self.vars_dict['x2_1']
 
-        ek = [x1, y1]  # Point A
-        fk = [x1, y2]  # Point C
-        hk = [x2, y1]  # Point B
-        gk = [x2, y2]  # Point D
-        fek = self.example_function_n2(x1, y1)
-        ffk = self.example_function_n2(x1, y2)
-        fhk = self.example_function_n2(x2, y1)
-        fgk = self.example_function_n2(x2, y2)  # function values at points A, B, C and D.
+        if minimum == fek:
+            print(f'Minimum at the point: ({x1_0},{x1_1})')
+        elif minimum == ffk:
+            print(f'Minimum at the point: ({x1_0},{x2_1})')
+        elif minimum == fhk:
+            print(f'Minimum at the point: ({x2_0},{x1_1}))')
+        elif minimum == fgk:
+            print(f'Minimum at the point: ({x2_0},{x2_1})')
 
-        min1 = None
-        while sqrt(pow((b1 - a1), 2) + pow((b2 - a2), 2)) > epsilon:  # termination condition
-            k = k + 1
-            min1 = min([fek, fhk, ffk, fgk])
-
-            if min1 == fek:
-                b1 = x2
-                b2 = y2
-            elif min1 == ffk:
-                b1 = x2
-                a2 = y1
-            elif min1 == fgk:
-                a1 = x1
-                a2 = y1
-            elif min1 == fhk:
-                a1 = x1
-                b2 = y2
-
-            x1 = a1 + (1 - tau) * (b1 - a1)
-            x2 = a1 + tau * (b1 - a1)
-            y1 = a2 + (1 - tau) * (b2 - a2)
-            y2 = a2 + tau * (b2 - a2)
-            fek = self.example_function_n2(x1, y1)
-            ffk = self.example_function_n2(x1, y2)
-            fhk = self.example_function_n2(x2, y1)
-            fgk = self.example_function_n2(x2, y2)
-            min1 = min([fek, fhk, ffk, fgk])
-
-        if min1 == fek:
-            print(f'minimum at the point ({x1},{y1})')
-        elif min1 == ffk:
-            print(f'minimum at the point ({x1},{y2})')
-        elif min1 == fhk:
-            print(f'minimum at the point ({x2},{y1}))')
-        elif min1 == fgk:
-            print(f'minimum at the point ({x2},{y2})')
-
-        print(f'minimum value {min1}')
-        print(f'number of iterations {k}')
+        print(f'Minimum value: {minimum}')
+        print(f'Number of iterations: {self.iteration}')
 
     @staticmethod
     def example_function_n1(x):
@@ -170,6 +115,5 @@ class Algorithm:
 
 if __name__ == '__main__':
     alg = Algorithm([0, 0], [1, 1], 2)
-    # print('Szukany punkt:', alg.find_minimum_value_n1())
-    # print('Szukany punkt:', alg.find_minimum_value_n2())
-    alg.find_minimum_matlab()
+    # alg.find_minimum_value_n1()
+    alg.find_minimum_value_n2()
