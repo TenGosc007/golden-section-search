ca4c6d82bad8074bbfc07d2e4a3a7c4a7119cafb
Damian Schmidt <damian.schmidt97@gmail.com>
2020-05-27 19:45:25 +0200

    Add contour plotting

--- src/plot_field.py
+++ src/plot_field.py
@@ -38,8 +38,8 @@ class PlotField:
                 for j in range(len(y_surf[0])):
                     z_surf[i, j] = self.math_interpreter.calculate([x_surf[0, i], y_surf[j, 0]])
 
-            ax.plot_surface(x_surf, y_surf, z_surf, rstride=1, cstride=1, cmap='viridis_r', alpha=.9)
-
+            ax.plot_surface(x_surf, y_surf, z_surf, rstride=1, cstride=1, cmap='viridis_r', alpha=.5)
+            z_min = np.amin(z_surf)
             size = abs(float(x2) - float(x1)) / 50
             xx, yy = self.window.algorithm.min_value
             zz = self.math_interpreter.calculate([xx, yy])
@@ -47,6 +47,7 @@ class PlotField:
             ax.add_patch(p)
             art3d.pathpatch_2d_to_3d(p, z=zz, zdir="z")
 
+            ax.contour(x_surf, y_surf, z_surf, zdir='z', offset=z_min, cmap='viridis_r')
             ax.set_xlabel('x1')
             ax.set_ylabel('x2')
             ax.set_zlabel('y')
